d8fa437c41ec87ea437ce59fcc363257ab5a5f81
Ahmad Shahzad <ahnadshahzad10119@gmail.com>
2026-04-13 13:35:24 +0100

    Loan Class Finished - Loan Table Created in Database

---
+++ LibraraySystem/FormLoanBook.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace LibraraySystem
+{
+    partial class FormLoanBook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // FormLoanBook
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1251, 665);
+            Name = "FormLoanBook";
+            Text = "Loan Book";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ LibraraySystem/Loan.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraraySystem
+{
+    class Loan
+    {
+        public int LoanID {  get; set; }
+        public Member Member { get; set; }
+        public Book Book { get; set; }
+        public DateTime StartDate {  get; set; }
+        public DateTime DueDate {  get; set; }
+        public DateTime? ReturnedDate {  get; set; }
+
+       
+
+        public Loan(int loanID, Member member, Book book, DateTime startDate, DateTime dueDate)
+        {
+            LoanID = loanID;
+            Member = member;
+            Book = book;
+            StartDate = startDate;
+            DueDate = dueDate;
+            ReturnedDate = null;
+        }
+
+        public override string ToString()
+        {
+            string str = "Loan Details:\n\n";
+
+            str += "Loan ID: " + LoanID +
+                   "\n\nMember Details:\n";
+
+            if (Member != null) {
+                str += Member.ToString();
+            }
+            else
+            {
+                str += "\nNo Member Found";
+            }
+            str += "\n\nBook Details:\n";
+
+            if (Book != null) {
+                str += Book.ToString();
+            }
+            else
+            {
+                str += "No Book Found";
+            }
+            str +="\nLoan Start Date: " + StartDate +
+                "\nLoan Due Date: " + DueDate +
+                "\nLoan Returned Date: " + ReturnedDate;
+
+            return str;
+        }
+    }
+}
